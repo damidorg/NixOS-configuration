018d6c7e36f4056df3ed141602d4091f27c9c375
damidorg <andigo1316@gmail.com>
2025-06-15 15:17:47 +0300

    some thing cool 3

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -1,1 +1,6 @@
 set fish_greeting
+zoxide init fish | source
+starship init fish | source
+# ~/.config/fish/config.fish
+set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense' # optional
+carapace _carapace | source
